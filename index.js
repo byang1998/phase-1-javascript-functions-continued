c549e87a183b58b6d1a39d895222fdb51ae21344
Ben Yang <benyang2020@gmail.com>
2021-08-31 19:17:54 -0400

    commit tree d27e8fe89d3fe0e8688fdcdcc3a19269d8b3136b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(defaultactivity="roller-skate") {
+ return(`This Saturday, I want to ${defaultactivity}!`);
+}
+
+const mondayWork = function(defaultactivity="go to the office") {
+    return(`This Monday, I will ${defaultactivity}.`);
+}
+
+function wrapAdjective(first= '*'){
+    return function innerFunction (str ="a hard worker"){
+     return `You are ${first}${str}${first}!`
+    }
+    
+    }
+
+
